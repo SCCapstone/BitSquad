4a32864e553dc81a2baffcc447dcb11aaa7b6f70
Kelli Alan <alankelli64@gmail.com>
2021-11-20 18:09:07 -0500

    Added form component

---
+++ src/app/process-form/process-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-process-form',
+  templateUrl: './process-form.component.html',
+  styleUrls: ['./process-form.component.scss']
+})
+export class ProcessFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
