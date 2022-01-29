be10644ebc017fd9b5118e6176408de35eb11993
zhiquan xie <69903223+zhiquan114514@users.noreply.github.com>
2022-01-28 22:32:36 -0500

    unit test, I guess

--- src/app/services/account-service.service.spec.ts
+++ src/app/services/account-service.service.spec.ts
@@ -13,4 +13,17 @@ describe('AccountServiceService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  
+  it('should get email', () =>{
+    service.setCurrentUser("1145191919@gmail.com")
+    expect(service.getCurrentUserEmail()).toContain("1145191919@gmail.com")
+  })
+  
+  it('should set through local storage', ()=> {
+    localStorage.setItem('email',"test")
+    localStorage.setItem('userID',"testID")
+    service.setViaLocalStorage()
+    expect(service.getCurrentUserEmail()).toContain("test")
+    expect(service.getUID()).toContain("testID")
+  })
 });
